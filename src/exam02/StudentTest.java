fc044a5a7a8b2c863dedde3fb5cc608233ab9c7d
ramses <ramses69@naver.com>
2024-02-18 13:50:05 +0900

    점수조정

--- src/exam02/StudentTest.java
+++ src/exam02/StudentTest.java
@@ -11,15 +11,15 @@ public class StudentTest {
 		Student student;
 		
 		student = new Student("Tony");
-		student.addSubject(88, 95, 70);
+		student.addSubject(88, 95, 80);
 		stList.add(student);
 		
 		student = new Student("Tiffany");
-		student.addSubject(90, 98, 80);
+		student.addSubject(90, 93, 85);
 		stList.add(student);
 		
 		student = new Student("Jennifer");
-		student.addSubject(78, 90, 80);
+		student.addSubject(83, 90, 86);
 		stList.add(student);		
 		
 		// 정렬
